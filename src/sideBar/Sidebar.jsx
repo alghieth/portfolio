8da9525efff69f32ff3106d72545202c88a0119a
alghieth <alghieth093094@gmail.com>
2026-09-09 15:20:28 +0200

    Edit Linkedin & Resume

--- src/sideBar/Sidebar.jsx
+++ src/sideBar/Sidebar.jsx
@@ -17,7 +17,7 @@ export default function Sidebars() {
             </a>
           </li>
           <li>
-            <a href="https://linkedin.com" target="_blank" rel="noreferrer" aria-label="LinkedIn">
+            <a href="www.linkedin.com/in/alghieth" target="_blank" rel="noreferrer" aria-label="LinkedIn">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                 <rect x="2" y="9" width="4" height="12"></rect>
