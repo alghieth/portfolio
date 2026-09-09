7b90ba7af0a67d6ac4b5f20decc5020a6f158d46
alghieth <alghieth093094@gmail.com>
2026-09-09 22:21:31 +0200

    make PWA App

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'alghieth-cache-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // تمرير الطلبات كالمعتاد مع استيفاء شرط PWA للتثبيت
+  event.respondWith(fetch(event.request));
+});
